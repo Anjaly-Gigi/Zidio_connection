6321a56ff507d639d18c29dd6fb4a64bef7d680f
Anjaly-Gigi <anjalygigi@gmail.com>
2025-07-24 17:41:59 +0530

    students

--- src/main/java/com/example/service/AuthService.java
+++ src/main/java/com/example/service/AuthService.java
@@ -1,4 +1,4 @@
-package com.example.services;
+package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
